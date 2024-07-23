10b60a3f2c4d3466965ff7e01ef6edee76fbd8b1
Yassine Laaguidi <aitlaaguidiyassine@gmail.com>
2024-07-23 08:07:26 +0200

    Handling pressable

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,23 +1,57 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
 
-function GoalItem(props) {
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+    const [courseGoals, setCourseGoals] = useState([]);
+
+    function addGoalHandler(enteredGoalText) {
+        setCourseGoals((currentCourseGoals) => [
+            ...currentCourseGoals,
+            { text: enteredGoalText, id: Math.random().toString() },
+        ]);
+    }
+
+    function deleteGoalHandler(id) {
+        setCourseGoals((currentCourseGoals) => {
+            return currentCourseGoals.filter((goal) => goal.id !== id);
+        });
+    }
+
     return (
-        <View style={styles.goalItem}>
-            <Text style={styles.goalText}>{props.text}</Text>
+        <View style={styles.appContainer}>
+            <GoalInput onAddGoal={addGoalHandler} />
+            <View style={styles.goalsContainer}>
+                <FlatList
+                    data={courseGoals}
+                    renderItem={(itemData) => {
+                        return (
+                            <GoalItem
+                                text={itemData.item.text}
+                                id={itemData.item.id}
+                                onDeleteItem={deleteGoalHandler}
+                            />
+                        );
+                    }}
+                    keyExtractor={(item, index) => {
+                        return item.id;
+                    }}
+                    alwaysBounceVertical={false}
+                />
+            </View>
         </View>
     );
 }
 
-export default GoalItem;
-
 const styles = StyleSheet.create({
-    goalItem: {
-        margin: 8,
-        padding: 8,
-        borderRadius: 6,
-        backgroundColor: '#5e0acc',
+    appContainer: {
+        flex: 1,
+        paddingTop: 50,
+        paddingHorizontal: 16,
     },
-    goalText: {
-        color: 'white',
+    goalsContainer: {
+        flex: 5,
     },
 });
